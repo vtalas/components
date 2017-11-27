dfcc6393e2bb8bba414d812c4b3c4c63d678da18
vladimir talas <vladimir.talas@gmail.com>
2017-11-27 23:00:19 +0100

    logo

--- a/logo/logo.js
+++ a/logo/logo.js
@@ -9,11 +9,29 @@ export const logoProto = {
         let bottom = this.dom.bottom = sourceElement.querySelector('.bottom');
         let right = this.dom.right = sourceElement.querySelector('.right');
         let path = this.dom.path = sourceElement.querySelector('#path3438');
+        let g = this.dom.g = sourceElement.querySelector('#layer2');
 
         var o = { x: 1.5, y: 3.9 };
         var w = 93.2;
         var h = 24.1;
         var stroke = 2.6;
+
+        var square = 5;
+        let center = { x: 46, y: 10 };
+
+        var ddd = {
+            left: ['M', center.x - square - stroke / 2, center.y + square + stroke, 'V', center.y + square],
+            right: ['M', center.x + square - stroke / 2, center.y - square, 'V', center.y - square],
+            top: ['M', center.x - square - stroke, o.y, 'H', center.x - square - stroke],
+            bottom: ['M', center.x + square, center.y + square, 'H', center.x + square]
+        };
+
+        var dd = {
+            left: ['M', center.x - square - stroke / 2, center.y + square + stroke / 2, 'V', center.y - square - stroke / 2],
+            right: ['M', center.x + square - stroke / 2, center.y - square, 'V', center.y + square],
+            top: ['M', center.x + square, o.y, 'H', center.x - square - stroke],
+            bottom: ['M', center.x - square - stroke, center.y + square, 'H', center.x + square]
+        };
 
         var d = {
             left: ['M', o.x + stroke / 2, o.y + h, 'V', o.y],
@@ -22,131 +40,75 @@ export const logoProto = {
             bottom: ['M', o.x, o.y + h, 'H', o.x + w]
         };
 
-        top.setAttribute('d', d.top.join(' '));
+        top.setAttribute('d', ddd.top.join(' '));
         top.setAttribute('stroke-width', stroke + '');
 
-        bottom.setAttribute('d', d.bottom.join(' '));
+        bottom.setAttribute('d', ddd.bottom.join(' '));
         bottom.setAttribute('stroke-width', stroke + '');
 
-        right.setAttribute('d', d.right.join(' '));
+        right.setAttribute('d', ddd.right.join(' '));
         right.setAttribute('stroke-width', stroke + '');
 
-        left.setAttribute('d', d.left.join(' '));
+        left.setAttribute('d', ddd.left.join(' '));
         left.setAttribute('stroke-width', stroke + '');
 
         const line = anime.timeline();
-        // anime({
-        //     target: this.dom.el,
-        //     scaleX: 0.01,
-        //     scaleY: 0.01,
-        //     transformOrigin: 'center'
-        //
-        // });
+        line.add({
+            targets: g,
+            opacity: 1,
+            duration: 1500,
+            easing: 'linear',
+        });
 
-        return ;
-        const duration = 500;
+        let xx = 100
+        const durDraw = (el, i) => {
+            xx += 100;
+            console.log(arguments);
+            return xx;
+        };
+        const easDraw = 'easeInOutExpo';
+        const offsetDraw = '-=20';
+        line
+            .add({ offset: offsetDraw, duration: durDraw, easing: easDraw, targets: left, d: dd.left.join(' ') })
+            .add({ offset: offsetDraw, duration: durDraw, easing: easDraw, targets: top, d: dd.top.join(' ') })
+            .add({ offset: offsetDraw, duration: durDraw, easing: easDraw, targets: right, d: dd.right.join(' ') })
+            .add({ offset: offsetDraw, duration: durDraw, easing: easDraw, targets: bottom, d: dd.bottom.join(' ') });
+
+        const offsetA = 2600;
+        const durationA = 1400;
+        const easA = 'easeInOutExpo';
+        line.add({ targets: right, duration: durationA, offset: offsetA, d: d.right.join(' '), easing: easA })
+            .add({ targets: top, duration: durationA, offset: offsetA, d: d.top.join(' '), easing: easA })
+            .add({ targets: bottom, duration: durationA, offset: offsetA, d: d.bottom.join(' '), easing: easA })
+            .add({ targets: left, duration: durationA, offset: offsetA, d: d.left.join(' '), easing: easA });
+
+        const offsetB = 3900;
         const elasticity = 100;
-
+        const durationB = 1500;
         d.right[4] = o.y;
         line.add({
-                targets: right,
-                duration: duration,
-                offset: duration / 4 * 1,
-                d: d.right.join(' '),
-                easing: 'easeOutQuad'
-            }
-        );
+            targets: right, duration: durationB / 4, offset: offsetB, d: d.right.join(' '), easing: 'easeOutQuad'
+        });
 
         d.left[4] = o.y + h;
         line.add({
-                targets: left,
-                duration: duration,
-                offset: duration / 4 * 2,
-                d: d.left.join(' '),
-                easing: 'easeOutQuad'
-            }
-        );
+            targets: left, duration: durationB / 4, offset: offsetB, d: d.left.join(' '), easing: 'easeOutQuad'
+        });
 
         d.top[4] = 62.5;
         line.add({
-                targets: top,
-                duration: duration,
-                offset: duration / 4 * 3,
-                elasticity: elasticity,
-                d: d.top.join(' ')
-            }
-        );
+            targets: top, duration: durationB, offset: offsetB, elasticity: elasticity, d: d.top.join(' ')
+        });
 
         d.bottom[4] = 30.7;
         line.add({
-                targets: bottom,
-                duration: duration,
-                offset: duration / 4 * 4,
-                elasticity: elasticity,
-                d: d.bottom.join(' ')
-            }
-        );
+            targets: bottom, duration: durationB, offset: offsetB, elasticity: elasticity, d: d.bottom.join(' ')
+        });
 
         line.add({
-                targets: path,
-                duration: 1000,
-                offset: duration / 16,
-                'stroke-width': 0,
-                easing: 'easeInExpo',
-            }
-        );
+            targets: path, duration: 1000, offset: offsetB, 'stroke-width': 0, easing: 'easeInExpo',
+        });
 
         return this;
     }
 };
-
-/*
-
-var top = $('.top');
-var bottom = $('.bottom');
-var left = $('.left');
-var right = $('.right');
-
-var d = {
-    left: ['M', o.x + sw / 2, o.y + h, 'V', o.y],
-    right: ['M', o.x + w - sw / 2, o.y, 'V', o.y + h],
-    top: ['M', o.x + w, o.y, 'H', o.x],
-    bottom: ['M', o.x, o.y + h, 'H', o.x + w]
-};
-top.attr({ d: d.top.join(' '), 'stroke-width': sw });
-bottom.attr({ d: d.bottom.join(' '), 'stroke-width': sw });
-right.attr({ d: d.right.join(' '), 'stroke-width': sw });
-left.attr({ d: d.left.join(' '), 'stroke-width': sw });
-var ss = 1;
-
-var ease = Power4.easeOut;
-var tl = new TimelineLite();
-tl.startTime(1);
-tl.add(TweenLite.from($('.logo'), 0.5, {
-    scaleX: 0.01,
-    scaleY: 0.01,
-    transformOrigin: 'center',
-    ease: Expo.easeIn
-}));
-
-tl.add('end');
-var rightto = d.right.slice();
-rightto[4] = o.y;
-tl.add(TweenLite.to(right, ss, { attr: { 'd': rightto.join(' '), }, ease: ease }), 'end');
-
-var leftto = d.left.slice();
-leftto[4] = o.y + h;
-tl.add(TweenLite.to(left, ss, { attr: { 'd': leftto.join(' '), }, ease: ease }), 'end');
-
-tl.add('aaa');
-var topto = d.top.slice();
-topto[4] = 62.5;
-tl.add(TweenLite.to(top, ss, { attr: { 'd': topto.join(' '), }, ease: ease }), 'end');
-
-var bottomTo = d.bottom.slice();
-bottomTo[4] = 30.7;
-tl.add(TweenLite.to(bottom, ss, { attr: { 'd': bottomTo.join(' '), }, ease: ease }), 'end');
-
-tl.add(TweenLite.to($('#path3438'), ss, { 'stroke-width': 0, ease: Power4.easeOut }));
-
-*/
