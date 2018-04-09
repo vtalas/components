ec4e2312a850ade18ac937ac982eca271b6f8b48
vladimir talas <vladimir.talas@gmail.com>
2018-04-09 09:05:16 +0200

    calc

--- calc/src/index.js
+++ calc/src/index.js
@@ -18,22 +18,68 @@ const Render = Object.create({
         return this;
     },
 
-    add(tag, label, value) {
+    add(tag, value) {
         const x = document.createElement(tag);
-        x.textContent = `${label} \t ${value || ''}`;
-        this.el.appendChild(x);
+        if (typeof  value === 'string') {
+            x.textContent = `${value}`;
+        } else if (Array.isArray(value)) {
+            value.forEach(function(item) {
+                x.appendChild(item)
+            })
+        } else {
+            x.appendChild(value);
+        }
+
+        return this.el.appendChild(x);
+    },
+
+    textEl(tag, text, attrs) {
+        const x = document.createElement(tag);
+        x.textContent = text;
+        Object.keys(attrs || {}).forEach(function(item) {
+            x.setAttribute(item, attrs[item])
+        });
+        return x;
     },
 
     render() {
 
         const r = this.result;
-        this.add('span', 'castka');
-        this.add('h3',  (r.sum.begin).toLocaleString());
+        this.add('h4', '---------------------------------------------------------');
+        this.add('h3', `${(r.meta.interest).toLocaleString()}% / ${r.meta.months} měsíců / splatka: ${(r.meta.payment).toLocaleString()} Kc`);
+        // this.add('h3', 'urok', (r.meta.interest).toLocaleString() + '%');
+        // this.add('h3', 'pocet mesicu', (r.meta.months).toLocaleString());
+        // this.add('h3', 'splatka', (r.meta.payment).toLocaleString());
+        this.add('h4', '---------------------------------------------------------');
 
-        this.add('h3', 'urok', (r.meta.interest).toLocaleString() + '%');
-        this.add('h3', 'pocet mesicu', (r.meta.months).toLocaleString());
-        this.add('h3', 'splatka', (r.meta.payment).toLocaleString());
-        this.add('h3', '---------------------------------------------------------');
+        this.add('span', 'jistina');
+        this.add('h3', (r.sum.begin).toLocaleString());
+        this.add('h3', (r.sum.end).toLocaleString());
+
+        this.add('h4', ' ');
+        this.add('span', 'zaplaceno jistiny celkem');
+        this.add('h3', (r.sum.begin - r.sum.end).toLocaleString());
+        this.add('h4', ' ');
+        this.add('span', 'zaplaceny urok celkem');
+        this.add('h3', (r.sum.interestYearly).toLocaleString());
+        this.add('h4', ' ');
+        this.add('div', '---------------------------------------------------------');
+
+        const tableHeader = this.add('div', [this.textEl('span', '#splatka', { class: 's_1' }), this.textEl('span', 'urok'), this.textEl('span', 'jistina')]);
+        tableHeader.setAttribute('class', 'list');
+        this.add('div', '---------------------------------------------------------');
+        for (var i = 0; i < r.payments.length; i++) {
+            var a = r.payments[i];
+
+            const tableContent = this.add('div', [
+                this.textEl('span', i + 1, { class: 's_1' }),
+                this.textEl('span', (a.rate).toLocaleString()),
+                this.textEl('span', (a.payment).toLocaleString())
+            ]);
+            tableContent.setAttribute('class', 'list')
+        }
+
+        console.log(r);
     }
 });
 
